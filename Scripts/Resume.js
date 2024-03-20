b9b1bdd1a7c2f000ef93083c3eee119231a6ba58
Hojjat-hk <minhojat@gmail.com>
2024-03-20 17:05:40 +0330

    Develop Resume Section Scripts

---
+++ Scripts/Resume.js
@@ -0,0 +1,5 @@
+// [+] Variables Defining
+const resumeSectionButtons = $.querySelectorAll(".resume-list__item");
+
+// [+] Send data to Functions
+navigationTapsInit(resumeSectionButtons, "click", "resume-list__item--active", "resume-content--show");
